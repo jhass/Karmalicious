584404452da628a50778a05805712252816ef966
Jonne Haß <me@jhass.eu>
2014-09-18 14:35:41 +0200

    silent mode

--- bot.rb
+++ bot.rb
@@ -9,31 +9,40 @@ class Cinch::User
   include UserMethods
 end
 
+ADMINS = %w(jhass MrZYX DenSchub)
+BLACKLIST = %w(c C)
+
+def authorized? m
+  ADMINS.include?(m.user.nick) && m.channel.nil?
+end
+
 bot = Cinch::Bot.new do
   configure do |c|
     c.nick = "Karmalicious"
     c.realname = "karmalicious.mrzyx.de"
     c.user = "karma"
-    c.server = "barjavel.freenode.net"
+    c.server = "irc.freenode.net"
     c.port = 6667
     c.channels = ChannelList.to_a
   end
-  
+
   NICK_REGEX = /[\w\d\-_\^'´`]+/
-  
+
   on :message, /^(#{NICK_REGEX})(\+\+|\-\-)/ do |m, nick, inc_or_dec|
     method = (inc_or_dec == "++") ? :increase_karma_of : :decrease_karma_of
+    break if BLACKLIST.include?(nick)
     user = User(nick)
     valid_user = user && user.nick != bot.nick && m.channel.has_user?(user)
-    unless valid_user && m.user.__send__(method, user, m.channel.name)
+    unless valid_user && m.user.public_send(method, user, m.channel.name)
+      break if ChannelList[m.channel.name].silent?
       m.user.send "You can't modify the karma of #{nick} because #{nick} either isn't in the channel, a bot, that's your own name or you already did in the last five minutes."
     end
   end
-  
+
   on :message, /^!karma$/ do |m|
     m.reply m.user.karma_string
   end
-  
+
   on :message, /^!karma\s+(#{NICK_REGEX})$/ do |m, nick|
     user = User(nick)
     if user.nick == bot.nick
@@ -42,37 +51,50 @@ bot = Cinch::Bot.new do
       m.reply user.karma_string if user
     end
   end
-  
+
   CHANNEL_REGEX = /#[\w\d_\-]+/
-  
+
   on :message, /^!join\s+(#{CHANNEL_REGEX})/ do |m, channel|
-    if m.channel.nil?
-      synchronize(:joinpart) do
-        unless bot.channels.include?(channel)
-          ChannelList.join channel
-          bot.join channel
-        end
+    break unless authorized?(m)
+    synchronize(:joinpart) do
+      unless bot.channels.include?(channel)
+        ChannelList.join channel
+        bot.join channel
       end
     end
   end
-  
+
   on :message, /^!part\s+(#{CHANNEL_REGEX})/ do |m, channel|
-    if m.channel.nil?
-      synchronize(:joinpart) do
-        if bot.channels.include?(channel)
-          ChannelList.part channel
-          bot.part channel
-        end
+    break unless authorized?(m)
+    synchronize(:joinpart) do
+      if bot.channels.include?(channel)
+        ChannelList.part channel
+        bot.part channel
       end
     end
   end
-  
+
+  on :message, /^!silent\s+(#{CHANNEL_REGEX})/ do |m, name|
+    break unless authorized?(m)
+    channel = ChannelList[name]
+    if channel.nil?
+      m.reply "I'm not in #{name}."
+    elsif channel.silent?
+      channel.verbose!
+      m.reply "I'll be verbose in #{channel.name} from now on."
+    else
+      channel.silent!
+      m.reply "I'll be silent in #{channel.name} from now on."
+    end
+  end
+
   on :message, /^#{config.nick}(?:\:|,|\s+)\s*(?:about|help|who a?re? (yo)?u|shut (the)? (fuck)? up|stfu|hello|hi|welcome|go( away)?|leave|part|join|come( to)?|site|website|page|webpage|info|man|gtfo).*/ do |m|
+    break if ChannelList[m.channel.name].silent?
     m.reply "Hi, I'm Karmalicious, I keep track of your karma. If you want to know more about me visit http://karmalicious.mrzyx.de"
   end
 end
 
 bot.loggers.level = :info
-DB.sql_log_level = :debug
+DB.sql_log_level = :info
 DB.loggers << bot.loggers.first
 bot.start
--- channel_list.rb
+++ channel_list.rb
@@ -1,61 +1,112 @@
 require 'yaml'
 
 class ChannelList
-  module ClassMethods
+  Channel = Struct.new(:name, :silent) do
+    alias_method :silent?, :silent
+
+    def silent!
+      self.silent = true
+      ChannelList.list.store!
+    end
+
+    def verbose!
+      self.silent = false
+      ChannelList.list.store!
+    end
+
+    def ==(other)
+      other.respond_to?(:name) && name == other.name
+    end
+
+    def hash
+      name.hash
+    end
+  end
+
+  FILENAME = 'channels.yml'.freeze
+
+  class << self
     def to_a
       list.to_a
     end
-    
+
     def add(chan)
       list.add(chan)
     end
     alias_method :join, :add
     alias_method :<<, :add
-    
+
     def rm(chan)
       list.rm(chan)
     end
     alias_method :part, :rm
     alias_method :delete, :rm
 
+    def [](name)
+      list[name]
+    end
+
     def reload!
       list.reload!
       self
-    end    
+    end
 
     def list
-      @@list ||= self.new
+      @list ||= new
     end
   end
-  extend ClassMethods
-  
+
+  include MonitorMixin
+
   def initialize
-    self.store! unless File.exists?('channels.yml')
+    super
+    store! unless File.exists? FILENAME
     load!
   end
-  
+
   def load!
-    @channels = YAML.load_file "channels.yml"
+    synchronize do
+      @channels = YAML.load_file FILENAME
+    end
+    migrate! unless @channels.first.is_a? Channel
   end
   alias_method :reload!, :load!
 
   def store!
-    open('channels.yml', 'w') do |f|
-      f.write YAML.dump @channels || []
+    synchronize do
+      open('channels.yml', 'w') do |f|
+        f.write YAML.dump @channels || []
+      end
     end
   end
-  
-  def add(chan)
-    @channels.push(chan) unless @channels.include?(chan)
-    self.store!
+
+  def add(name)
+    synchronize do
+      channel = self[name]
+      @channels.push(Channel.new(name, false)) unless channel
+    end
+    store!
   end
-  
+
   def rm(chan)
-    @channels.delete(chan)
-    self.store!
+    synchronize do
+      @channels.delete(self[name])
+    end
+    store!
   end
-  
+
+  def [](name)
+    @channels.find {|channel| channel.name == name }
+  end
+
+  def migrate!
+    synchronize do
+      @channels = YAML.load_file(FILENAME).map {|name| Channel.new(name, false) }
+    end
+    store!
+  end
+
   def to_a
-    @channels
+    @channels.map(&:name)
   end
 end
